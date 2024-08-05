6a752813e706ea75f94639a0790d4b4fdb3adf50
Shazil Hamzah <shazil.hamzah@outlook.com>
2024-08-06 00:58:24 +0500

    tictactoe complete

--- Week 0/tictactoe/tictactoe.py
+++ Week 0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -43,11 +44,11 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    tupleToReturn = []
+    tupleToReturn = set()
     for i in range(0,3):
         for j in range(0,3):
             if board[i][j]==EMPTY:
-                tupleToReturn.append((i,j))
+                tupleToReturn.add((i,j))
     return tupleToReturn
     raise NotImplementedError
 
@@ -56,16 +57,12 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    turn = player(board)
-    a = action[0]
-    b = action[1]
-    newBoard = [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
-    for i in range(0,3):
-        for j in range(0,3):
-            newBoard[i][j] = board[i][j]
-    newBoard[a][b] = turn;
+    if action not in actions(board):
+        print(action)
+        raise Exception("Invlid action")
+    row,col = action
+    newBoard = copy.deepcopy(board)
+    newBoard[row][col] = player(board);
     return newBoard
     raise NotImplementedError
 
@@ -75,22 +72,12 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
     for i in range(0,3):
-        similar = board[i][0]
-        count=1;
-        for j in range(0,3):
-            if board[i][j]==similar:
-                count+=1
-        if(count==3):
+        if board[i][0]==board[i][1] and board[i][1]==board[i][2]:
             return board[i][0]
         
     for j in range(0,3):
-        similar = board[0][j]
-        count=1;
-        for i in range(0,3):
-            if board[i][j]==similar:
-                count+=1
-        if(count==3):
-           return board[0][j]
+        if board[0][j]==board[1][j] and board[1][j]==board[2][j]:
+            return board[0][j]
         
     if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
         return board[1][1]
@@ -107,6 +94,8 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
+    if winner(board)==X or winner(board)==O:
+        return True
     for i in range(0,3):
         for j in range(0,3):
             if board[i][j]==EMPTY:
@@ -130,11 +119,50 @@ def utility(board):
     raise NotImplementedError
 
 
+def maxValue(board):
+    v = -math.inf
+    if terminal(board):
+        return utility(board)
+    for action in actions(board):
+        v = max(v,minValue(result(board,action)))
+    return v
+
+def minValue(board):
+    v = math.inf
+    if terminal(board):
+        return utility(board) 
+    for action in actions(board):
+        v = min(v,maxValue(result(board,action)))
+    return v
+
+
+
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
     if(terminal(board)):
         return None
+    whosPlaying = player(board)
+    if(whosPlaying==X):
+        bestValue = -math.inf
+        bestAction = None
+        for action in actions(board):
+            v = minValue(result(board,action))
+            if v>bestValue:
+                bestValue = v
+                bestAction = action
+        return bestAction
+    
+    elif(whosPlaying==O):
+        bestValue = math.inf
+        bestAction = None
+        for action in actions(board):
+            v = maxValue(result(board,action))
+            if v<bestValue:
+                bestValue = v
+                bestAction = action
+        return bestAction
+    
     
     raise NotImplementedError
